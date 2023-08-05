54f864002f7f9bd2380be1eaf4d15a6828519dbc
danjor667 <j.nguepi@alustudent.com>
2023-08-05 04:35:12 -0700

    adding solution

---
+++ Median_of_two_sorted_arrays/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def median(nums1: list, nums2: list) -> float:
+    """
+    >>> nums1 = [1, 2, 3, 4]
+    >>> nums2 = [6, 8, 10]
+    >>> print(median(nums1, nums2))
+    4.0
+    >>> num1 = [2,4,6,8,]
+    >>> nums2 = [3,5,7,10]
+    >>> print(median(nums1, nums2))
+    5.5
+    """
+    nums = nums1 + nums2
+    nums.sort()
+    n = len(nums) // 2
+    if len(nums) % 2 == 1:
+        return float(nums[n])
+    else:
+        return nums[n] + nums[n - 1] / 2
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
